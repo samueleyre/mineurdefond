651ddea21637f0e45d1524977d9b6ad02a28953f
Samuel EYRE <samueleyre@Samuels-MacBook-Air.local>
2021-07-28 13:23:08 +0200

    up fest page

--- components/festival/festival-event.component.js
+++ components/festival/festival-event.component.js
@@ -1,12 +1,47 @@
 
-export default function FestivalEventComponent({title, title_color = "black", place_and_time, musique_style, link, newPage = null}) {
+export default function FestivalEventComponent(
+  {
+    image=null,
+    title,
+    title_color = "black",
+    place_and_time,
+    musique_style,
+    link,
+    newPage = null,
+    description = "",
+    listenLink = null,
+    fbEvent = null
+  }
+) {
     return (
         <div className="text-center m-6" style={{width: "250px"}}>
+            <div
+            style={{
+              width: "250px",
+              height: "200px",
+              backgroundImage: `url(/images/${image})`,
+              backgroundSize: "cover",
+              backgroundRepeat: "no-repeat",
+              backgroundPosition: "bottom",
+              margin: "auto"
+            }}
+
+            />
             <a href={link} target={newPage ? "_blank" : "_self"} style={link ? {cursor: "pointer"} : {cursor: "default"}}>
-                <h5 className="text-4xl pb-4" style={{color: title_color}}>{title}</h5>
+                <h5 className="text-4xl pb-4 font-bold" style={{color: title_color}}>{title}</h5>
             </a>
             <p>{place_and_time}</p>
-            <p className="italic pt-1">{musique_style}</p>
+            <p className="italic pt-1" style={{color: "grey"}}>{musique_style}</p>
+            <p className="pt-4">
+            {description}
+            </p>
+            <div className="flex flex-col items-center justify-center">
+              { listenLink ?
+                <a className="underline mt-4" href={listenLink}>Écouter</a>
+                : ""
+               }
+              <a className="underline mt-2" href={fbEvent}>Plus d'infos/évènement Fb</a>
+            </div>
         </div>
     )
 }
--- pages/festival-des-pantomines.js
+++ pages/festival-des-pantomines.js
@@ -18,15 +18,16 @@ function FestivalDesPantomines() {
             concerts et spectacles détonnants !
             </p>
             <p className="text-xl mb-8 pl-4 pr-4">
-            En journée ou à la faveur des lucioles, ce festival vous invite à une plongée dans des paysages
-            exceptionnels autour de rencontres musicales insolites et fortes en émotion.
-            Toujours en pleine nature, chaque rendez-vous sera donné dans un lieu différent des communes
-            de Villaroger et Sainte-Foy. La programmation éclectique vous réserve de nombreuses surprises
-            avec le trio de musiques des Balkans Lesto Drom, une balade matinale menée par un guide
-            et suivie d’un concert du violoniste Fred Aurier, une envolée musicale chantée par le tandem
-            Tchoukadane, le spectacle Touffe de Spleen à l’énergie 100% féminine ou encore les chansons
-            folk de Siri Byrkjedal tout droit venue de Norvège, pour finir avec le grand bal de MaDam
-            RamDam offert sous les étoiles.
+            En journée ou à la faveur des lucioles, ce festival vous invite à plonger dans des paysages
+exceptionnels autour de rencontres musicales insolites et fortes en émotion. Toujours en pleine
+nature, chaque rendez-vous sera donné dans un lieu différent des communes de Villaroger et
+Sainte-Foy.
+            <br/>
+            La programmation éclectique réserve de nombreuses surprises avec le trio de musiques des
+Balkans Lesto Drom, une balade menée par un guide et suivie d’un concert du violoniste Frédéric
+Aurier, le tandem de chansonniers Tchoukadane, le spectacle Touffe de Spleen à l’énergie 100%
+féminine ou encore les chansons folk de Siri Byrkjedal tout droit venue de Norvège... pour finir
+avec le bal de MaDam RamDam offert sous les étoiles !
             </p>
             <p className="text-2xl mb-6 text-center mt-16 italic" style={{color: "grey"}}>
             Programme
@@ -35,62 +36,109 @@ function FestivalDesPantomines() {
             <div className="mobile:p-4 sm:p-22 flex flex-wrap justify-center">
 
                 <FestivalEventComponent
+                    image="lesto.jpg"
                     title="LESTO DROM"
                     title_color="#a76a53"
                     link="https://www.noe-clerc.com/lesto-drom"
                     newPage="true"
                     place_and_time="Les îles, Villaroger à 18h"
-                    musique_style="Musique des Blakans"
+                    musique_style="Musiques des Balkans"
+                    description="De la Turquie à la Bulgarie et roulé en caravane tzigane, le trio kaval kaval emportant avec lui les plus belles mélodies des Balkans."
+                    listenLink="https://www.youtube.com/watch?v=xHrT_ToheOg&amp;ab_channel=Lo%C3%AFcVx"
+                    fbEvent="https://fb.me/e/JqzSeUWK"
                 />
             </div>
             <p className="p-6 text-4xl">14 AOUT</p>
             <div className="mobile:p-4 sm:pt-22 sm:pb-22 sm:pl-28 sm:pr-28 flex flex-wrap justify-center">
                 <FestivalEventComponent
+                    image="fred.jpeg"
                     title="FRED AURIER & CO"
                     title_color="#a8834b"
                     link="https://quatuorbela.com/le-quatuor-bela/frederic-aurier/"
                     newPage="true"
                     place_and_time="Le Monal, Sainte-Foy à 11h"
-                    musique_style="Ballade - Concert"
+                    musique_style="Balade - Concert"
+                    description="Une balade avec un guide et quelques surprises sera suivie d&#39;un concert de l&#39;étonnant violoniste et compositeur Frédéric Aurier (Quatuor Bela, La Soustraction des Fleurs...)"
+                    fbEvent="https://fb.me/e/2vBJRj5hn"
                 />
                 <FestivalEventComponent
+                    image="tchouk.jpg"
                     title="TCHOUKADANE"
                     link="tchoukadane"
                     title_color="#73638d"
-                    place_and_time="La Gurraz, front de vanoise, Villaroger à 15h"
+                    place_and_time="La Gurraz, Villaroger (front de vanoise) à 15h"
                     musique_style="Chants - Guitares"
+                    description="De l'errance comme souffle de vie, ce tandem murmure en miroir et s'enivre en guitares. Ouvrant un autre rapport à l'espace, au son et au texte, Tchoukadane nous entraîne vers un conte musical singulier."
+                    listenLink="https://www.youtube.com/watch?v=dSCmDhRXe8g"
+                    fbEvent="https://fb.me/e/AgeH86F5"
                 />
                 <FestivalEventComponent
+                    image="touff.jpg"
                     title="TOUFFE DE SPLEEN"
                     title_color="#8cada0"
                     link="https://www.savoie.fr/web/sw_59525/Culture/Touffedespleen"
                     newPage="true"
                     place_and_time="La Masure, Sainte-Foy à 19h"
                     musique_style="Caravane - Théâtre"
+                    description="Après avoir collecté des témoignages sur le spleen et la mélancolie, ces trois acolytes nous invitent à un joyeux tête-à-tête autour de leur caravane -on a déjà oublié qu'on n'allait pas bien."
+                    fbEvent="https://fb.me/e/j8PTa8EDH"
                 />
 
             </div>
             <p className="p-6 text-4xl">15 AOUT</p>
             <div className="mobile:p-4 sm:p-22 flex flex-wrap justify-center">
                 <FestivalEventComponent
+                    image="siri.jpg"
                     title="SIRI BIRK"
                     title_color="#333d67"
                     link="http://www.siribyrkjedal.com/"
                     newPage="true"
-                    place_and_time="Le planay, Villaroger à 11h30"
+                    place_and_time="Le Planay, Villaroger à 11h"
                     musique_style="Folk"
+                    description="Siri a traversé l'Europe pour nous apporter de Norvège ses chansons folk aux mélodies claires et couleurs boréales."
+                    fbEvent="https://fb.me/e/Vnjcsbe4"
                 />
                 <FestivalEventComponent
+                    image="madam.jpg"
                     title="MADAM RAMDAM"
                     link="madamramdam"
                     title_color="#963332"
                     place_and_time="Planjo, Sainte-Foy à partir de 19h30"
                     musique_style="Grand Bal"
+                    description="Ces cinq compères vont nous faire guincher sous les étoiles : un bal haut en couleur et pour tous les goûts à rendre jaloux le meilleur des jukebox ! Buvette et restauration légère."
+                    listenLink="https://www.youtube.com/watch?v=CrXQrFW27xY&amp;ab_channel=MaDamRamDam"
+                    fbEvent="https://fb.me/e/6FQH4gfsx"
                 />
 
             </div>
+
+            <p className="text-xl underline text-center mt-8 mb-8 pl-4 pr-4">
+            <a className="p-4" style={{backgroundColor: "#242424", color: "white"}} href="/documents/programme2021.pdf" target="_blank">Télécharger le programme</a>
+            </p>
+
+
+            <p className="text-2xl mb-6 text-center mt-16 italic" style={{color: "grey"}}>
+            Infos pratiques
+            </p>
+
             <p className="text-center mt-16 mb-8 pl-4 pr-4">
-            Une proposition construite avec les communes de
+              Tous les spectacles sont à prix libre (mais conseillé 10€).
+            </p>
+            <p className="text-center mb-8 pl-4 pr-4">
+              Un camping sera mis à disposition : contactez-nous !
+            </p>
+            <p className="text-center bold mb-8 pl-4 pr-4">
+            Infos : <br/>
+            c.mineursdefond@gmail.com / 06.87.29.02.65
+            </p>
+
+
+            <p className="text-center">
+            Vous pouvez suivre l'évènement sur Facebook : <a href="https://fb.me/e/5hcGg9hG0" target="_blank">https://fb.me/e/5hcGg9hG0</a>
+            </p>
+
+            <p className="text-center mt-16 mb-8 pl-4 pr-4">
+              Une proposition construite avec les communes de
             Villaroger et Sainte-Foy et la communauté de commune
             de Haute-Tarentaise.
             </p>
