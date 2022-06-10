bb8025584cd89a13871c5214bea10cd99c45a15f
Samuel Eyre <samuel.eyre@hotmail.fr>
2022-06-10 23:19:59 +0200

    edition 2022

--- components/festival/festival-event.component.js
+++ components/festival/festival-event.component.js
@@ -14,33 +14,39 @@ export default function FestivalEventComponent(
   }
 ) {
     return (
-        <div className="text-center m-6" style={{width: "250px"}}>
-            <div
+        <div className="text-center m-6" style={{width: "400px", maxWidth: "100%"}}>
+            { image ?
+                <div
             style={{
-              width: "250px",
-              height: "200px",
+              width: "400px",
+              maxWidth: "100%",
+              height: "300px",
               backgroundImage: `url(/images/${image})`,
               backgroundSize: "cover",
               backgroundRepeat: "no-repeat",
               backgroundPosition: "bottom",
               margin: "auto"
             }}
+                />
+                : ""
+            }
 
-            />
             <a href={link} target={newPage ? "_blank" : "_self"} style={link ? {cursor: "pointer"} : {cursor: "default"}}>
                 <h5 className="text-4xl pb-4 font-bold" style={{color: title_color}}>{title}</h5>
             </a>
             <p>{place_and_time}</p>
             <p className="italic pt-1" style={{color: "grey"}}>{musique_style}</p>
-            <p className="pt-4">
-            {description}
+            <p className="pt-4" dangerouslySetInnerHTML={{__html: description}}>
             </p>
             <div className="flex flex-col items-center justify-center">
               { listenLink ?
-                <a className="underline mt-4" href={listenLink}>Écouter</a>
+                <a className="underline mt-4" target="_blank" href={listenLink}>Écouter</a>
                 : ""
                }
-              <a className="underline mt-2" href={fbEvent}>Plus d'infos/évènement Fb</a>
+               { fbEvent ?
+                <a className="underline mt-2" target="_blank" href={fbEvent}>Plus d'infos/évènement Fb</a>
+                : ""
+               }
             </div>
         </div>
     )
