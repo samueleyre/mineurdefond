474f7879ab225ca6c59c68ec30fe8c7946399012
Samuel Eyre <samuel.eyre@hotmail.fr>
2022-09-26 14:27:42 +0200

    edition mode menu

--- components/main/nav.component.js
+++ components/main/nav.component.js
@@ -19,23 +19,23 @@ function rotateLight() {
     }
 }
 
-function activateMenu() {
+function toggleMenu() {
     rotate();
     rotateLight();
-    if (menuActivated) {
-        if (subMenuActivated) {
-            toggleSubMenu();
-        }
+    if (menuActivated && subMenuActivated) {
+        document.getElementById('nav-menu-main-items').classList.add('nav-menu-main-items--open')
+        Array.from(document.getElementsByClassName(`nav-menu-sub-items`)).forEach(e => e.classList.remove(`nav-menu-sub-items--open`))
     }
     menuActivated = !menuActivated;
     document.getElementById('nav-menu-items-wrapper').classList.toggle('nav-menu-items-wrapper--open')
 }
 
-function toggleSubMenu() {
+function toggleSubMenu(menu_id) {
     subMenuActivated = !subMenuActivated;
     document.getElementById('nav-menu-main-items').classList.toggle('nav-menu-main-items--open')
-    document.getElementById('nav-menu-sub-items').classList.toggle('nav-menu-sub-items--open')
+    document.getElementById(`nav-menu-sub-items-${menu_id}s`).classList.toggle(`nav-menu-sub-items--open`)
 }
+
 
 export default function NavComponent({mode, sm_mode, position, mobile_only = false}) {
 
@@ -46,14 +46,6 @@ export default function NavComponent({mode, sm_mode, position, mobile_only = fal
     const router = useRouter();
 
     const navs = [
-        {
-            name: "Accueil",
-            link: '/'
-        },
-        {
-            name: "Festival",
-            link: '/festival-des-pantomines'
-        },
         {
             name: "Histoire",
             link: '/histoire'
@@ -91,6 +83,18 @@ export default function NavComponent({mode, sm_mode, position, mobile_only = fal
         },
     ].filter((nav => nav.link !== router.pathname));
 
+    const editions_nav = [
+        {
+            name: "Edition 2022",
+            link: '/festival-des-pantomines'
+        },
+        {
+            name: "Edition 2021",
+            link: '/festival-des-pantomines-2021'
+        },
+    ].filter((nav => nav.link !== router.pathname));
+
+
     const mobile = window.innerWidth < 768;
     if (!sm_mode) {
         sm_mode = mode;
@@ -100,7 +104,7 @@ export default function NavComponent({mode, sm_mode, position, mobile_only = fal
         {!mobile ? !mobile_only ? (
         <div
             className={`${position} nav-menu ${mode === "light" ? "nav-menu--light" : "nav-menu--dark"} ${sm_mode === "light" ? "mobile-nav-menu--light" : "mobile-nav-menu--dark" } mt-4 pl-4 desktop mobileToTabloid:flex z-50`}>
-            <div id="nav-menu-icon" onClick={activateMenu} className="nav-menu-icon">
+            <div id="nav-menu-icon" onClick={toggleMenu} className="nav-menu-icon">
                 <svg
                     width="36.011681mm"
                     height="14.363094mm"
@@ -138,19 +142,30 @@ export default function NavComponent({mode, sm_mode, position, mobile_only = fal
                 <div id="nav-menu-main-items"
                      className="nav-menu-main-items nav-menu-main-items--open nav-menu-items absolute mobileToTabloid:mb-2 mobileToTabloid:flex-row mobileToTabloid:overflow-x-auto">
 
+                    <div key="0" className="nav-menu-item">
+                        <Link href="/">Accueil</Link>
+                    </div>
+                    <div className="nav-menu-item">
+                        <a href="#" onClick={() => toggleSubMenu("project")}>
+                            <span className="pr-1">Projets</span>
+                            <FontAwesomeIcon className="nav-menu-item-projets w-3" icon={faAngleRight}/>
+                        </a>
+                    </div>
+                    <div className="nav-menu-item">
+                        <a href="#" onClick={() => toggleSubMenu("edition")}>
+                            <span className="pr-1">Festival</span>
+                            <FontAwesomeIcon className="nav-menu-item-projets w-3" icon={faAngleRight}/>
+                        </a>
+                    </div>
+
                     {navs.map((nav, index) => {
                         return <div key={index} className="nav-menu-item">
                             <Link href={nav.link}>{nav.name}</Link>
                         </div>
                     })}
-                    <div className="nav-menu-item">
-                        <a href="#" onClick={toggleSubMenu}>
-                            <span className="pr-1">Projets</span>
-                            <FontAwesomeIcon className="nav-menu-item-projets w-3" icon={faAngleRight}/>
-                        </a>
-                    </div>
+
                 </div>
-                <div id="nav-menu-sub-items"
+                <div id="nav-menu-sub-items-projects"
                      className="nav-menu-sub-items nav-menu-items mobileToTabloid:mb-2 absolute mobileToTabloid:flex-row mobileToTabloid:overflow-x-auto">
                     {projects_nav.map((nav, index) => {
                         return <div key={index} className="nav-menu-item">
@@ -158,46 +173,83 @@ export default function NavComponent({mode, sm_mode, position, mobile_only = fal
                         </div>
                     })}
                     <div className="nav-menu-item">
-                        <a href="#" onClick={toggleSubMenu}>
+                        <a href="#" onClick={() => toggleSubMenu("project")}>
                             <FontAwesomeIcon className="nav-menu-item-retour w-3" icon={faAngleLeft}/>
                             <span className="pl-1">Retour</span>
                         </a>
                     </div>
                 </div>
+                <div id="nav-menu-sub-items-editions"
+                     className="nav-menu-sub-items nav-menu-items mobileToTabloid:mb-2 absolute mobileToTabloid:flex-row mobileToTabloid:overflow-x-auto">
+                    {editions_nav.map((nav, index) => {
+                        return <div key={index} className="nav-menu-item">
+                            <Link href={nav.link}>{nav.name}</Link>
+                        </div>
+                    })}
+                    <div className="nav-menu-item">
+                        <a href="#" onClick={() => toggleSubMenu("edition")}>
+                            <FontAwesomeIcon className="nav-menu-item-retour w-3" icon={faAngleLeft}/>
+                            <span className="pl-1">Retour</span>
+                        </a>
+                    </div>
+                </div>
+
             </div>
         </div>) : "" : (
         <div
             className={`nav-menu ${sm_mode === "light" ? "nav-menu--light" : "mobile-nav-menu--dark"} absolute w-full mobile z-50`}>
             <div id="nav-menu-items-wrapper" className="nav-menu-items-wrapper absolute bg-black flex w-full h-screen justify-center items-center">
                 <div id="nav-menu-main-items" className="nav-menu-main-items nav-menu-main-items--open nav-menu-items">
-
+                    <div key="0" className="nav-menu-item">
+                        <Link href="/">Accueil</Link>
+                    </div>
+                    <div className="nav-menu-item ">
+                        <a className="flex items-center" href="#" onClick={() => toggleSubMenu("project")}>
+                            <span className="pr-1">Projets</span>
+                            <FontAwesomeIcon className="w-3" icon={faAngleRight}/>
+                        </a>
+                    </div>
+                    <div className="nav-menu-item ">
+                        <a className="flex items-center" href="#" onClick={() => toggleSubMenu("edition")}>
+                            <span className="pr-1">Festival</span>
+                            <FontAwesomeIcon className="w-3" icon={faAngleRight}/>
+                        </a>
+                    </div>
                     {navs.map((nav, index) => {
                         return <div key={index} className="nav-menu-item">
                             <Link href={nav.link}>{nav.name}</Link>
                         </div>
                     })}
-                    <div className="nav-menu-item ">
-                        <a className="flex items-center" href="#" onClick={toggleSubMenu}>
-                            <span className="pr-1">Projets</span>
-                            <FontAwesomeIcon className="w-3" icon={faAngleRight}/>
-                        </a>
-                    </div>
                 </div>
-                <div id="nav-menu-sub-items" className="nav-menu-sub-items nav-menu-items">
+                <div id="nav-menu-sub-items-projects" className="nav-menu-sub-items nav-menu-items">
                     {projects_nav.map((nav, index) => {
                         return <div key={index} className="nav-menu-item">
                             <Link href={nav.link}>{nav.name}</Link>
                         </div>
                     })}
                     <div className="nav-menu-item nav-menu-item--return">
-                        <a className="flex items-center" href="#" onClick={toggleSubMenu}>
+                        <a className="flex items-center" href="#" onClick={() => toggleSubMenu("project")}>
                             <FontAwesomeIcon className="w-3" icon={faAngleLeft}/>
                             <span className="pl-1">Retour</span>
                         </a>
                     </div>
                 </div>
+                <div id="nav-menu-sub-items-editions" className="nav-menu-sub-items nav-menu-items">
+                    {editions_nav.map((nav, index) => {
+                        return <div key={index} className="nav-menu-item">
+                            <Link href={nav.link}>{nav.name}</Link>
+                        </div>
+                    })}
+                    <div className="nav-menu-item nav-menu-item--return">
+                        <a className="flex items-center" href="#" onClick={() => toggleSubMenu("edition")}>
+                            <FontAwesomeIcon className="w-3" icon={faAngleLeft}/>
+                            <span className="pl-1">Retour</span>
+                        </a>
+                    </div>
+                </div>
+
             </div>
-            <div id="nav-menu-icon" onClick={activateMenu} className="nav-menu-icon mt-4 ml-4 z-20 absolute">
+            <div id="nav-menu-icon" onClick={toggleMenu} className="nav-menu-icon mt-4 ml-4 z-20 absolute">
                 <svg
                     width="36.011681mm"
                     height="14.363094mm"
