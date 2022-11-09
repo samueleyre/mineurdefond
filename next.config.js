3e83a85fe24b6cfeecacf289bc3c2ac14142d1d5
Samuel Eyre <samuel.eyre@hotmail.fr>
2022-11-09 09:50:03 +0100

    update image domains

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 module.exports = {
     images: {
-        domains: ['dl.airtable.com']
+        domains: ['dl.airtable.com', 'v5.airtableusercontent.com', 'v6.airtableusercontent.com', 'v7.airtableusercontent.com', 'v8.airtableusercontent.com']
     }
 }
